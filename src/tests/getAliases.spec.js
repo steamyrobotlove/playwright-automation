580976f0ba630fc8b56cc863bb5f02c3e7c40aed
steamyrobotlove <steamyrobotlove@gmail.com>
2024-04-07 20:39:57 -0700

    getAliases.spec.js now deconstructs passed object, prints aliases to console

--- src/tests/getAliases.spec.js
+++ src/tests/getAliases.spec.js
@@ -1,6 +1,8 @@
 const { test, expect } = require('../modules/modules');
 
-test('Get Aliases', async ({ aTagsNoRedirect }) => {
-    console.log(aTagsNoRedirect);
-    // console.log(aTagsRedirect);
+test('Get aliases', async ({ aTagsNoRedirectAliases }) => {
+    const aliases = Object.values(aTagsNoRedirectAliases);
+    for (let alias of aliases) {
+        console.log(alias);
+    }
 });
