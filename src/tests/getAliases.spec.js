014028f7366abe765c0a923d0e0c7db2198c30f6
steamyrobotlove <steamyrobotlove@gmail.com>
2024-04-07 22:49:07 -0700

    added alias underscore validation, renamed script validateAliases.spec.js

--- src/tests/getAliases.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('../modules/modules');
-
-test('Get aliases', async ({ aTagsNoRedirectAliases }) => {
-    const aliases = Object.values(aTagsNoRedirectAliases);
-    for (let alias of aliases) {
-        console.log(alias);
-    }
-});
