4987f8d4b91f81f7365a103b4ae1da092236854d
steamyrobotlove <steamyrobotlove@gmail.com>
2024-04-07 23:14:14 -0700

    validateLinkResponses.spec.js

---
+++ src/tests/validateLinkResponses.spec.js
@@ -0,0 +1,15 @@
+const { playwright } = require('playwright');
+const { test, expect } = require('../modules/modules');
+
+test('Validate Link Responses', async ({ page, aTagsNoRedirectHrefs }) => {
+    const hrefs = Object.values(aTagsNoRedirectHrefs);
+    for (let href of hrefs) {
+        if (href && !href == "") {
+            const response = await page.goto(href, { waitUntil: 'load', timeout: 0 });
+            const status = response.status();
+            console.log(`Status of ${href} is ${status}`);
+        } else {
+            console.log(`Response of ${href} was weird`);
+        }
+    }
+});
