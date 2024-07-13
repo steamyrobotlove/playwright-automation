4735ff9d5866a1c12d764fe210e9c1cb0e44802b
steamyrobotlove <steamyrobotlove@gmail.com>
2024-07-13 13:11:56 -0700

    minor changes

--- src/tests/validateImages.test.js
+++ src/tests/validateImages.test.js
@@ -1,35 +1,39 @@
 import { test, expect } from '../fixtures/fixtures';
 
 test.describe('Validate images', () => {
-    // test('Validate image responses', async ({ page, getImgTagsSrcs }) => {
-    //     const srcs = Object.values(getImgTagsSrcs);
-    //     const trackingPixels = ['emltrk', 'emanalytics', 'ffcb10', 'two50'];
+    test('Validate image responses', async ({ page, getImgTagsSrcs }) => {
+        const srcs = Object.values(getImgTagsSrcs);
+        const trackingPixels = ['emltrk', 'emanalytics', 'ffcb10', 'two50'];
     
-    //     for (let src of srcs) {
-    //         if (trackingPixels.some((el) => src.includes(el))){
-    //             console.log(`${src} is a tracking pixel`)
-    //         } else if (src != '') {
-    //             const response = await page.goto(src, { waitUntil: 'load', timeout: 0 });
-    //             const status = response?.status();
+        for (let src of srcs) {
+            if (trackingPixels.some((el) => src.includes(el))){
+                console.log(`${src} is a tracking pixel`)
+            } else if (src != '') {
+                const response = await page.goto(src, { waitUntil: 'load', timeout: 0 });
+                const status = response?.status();
     
-    //             if (status == 200 || status == 301 || status == 302 || status == 304) {
-    //                 console.log(`Status of ${src} is ${status}`);
-    //             } else {
-    //                 console.log(`ERROR: status of ${src} is ${status}`);
-    //             }
-    //         } else {
-    //             continue;
-    //         }
-    //     }
-    // });
+                if (status == 200 || status == 301 || status == 302 || status == 304) {
+                    console.log(`Status of ${src} is ${status}`);
+                } else {
+                    console.log(`ERROR: status of ${src} is ${status}`);
+                }
+            } else {
+                continue;
+            }
+        }
+    });
 
-    test('Validate presence of alt text', async ({ getImgTags }) => {
-        const imgTags = Object.values(getImgTags);
-        const altValue = imgTags.forEach((imgTag) => {
-            const alt = imgTag.getAttribute('alt');
-            console.log(alt);
-        });
-        
+    test('Validate presence of alt text', async ({ getImgTagsSrcs, getImgTagsAlts }) => {
+        const imgSrcs = Object.values(getImgTagsSrcs);
+        const altValues = Object.values(getImgTagsAlts);
 
+        imgSrcs.forEach((src, i) => {
+            const alt = altValues[i];
+            if (alt) {
+                console.log(`Alt text found for ${src}\n Alt text: ${alt}`)
+            } else {
+                console.log(`No alt text found for ${src}`)
+            }
+        });       
     });
 });
